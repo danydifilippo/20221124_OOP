80d68cd0a9ec395497d5824b7caa6f36d750b5f7
danydifilippo <danydifilippo@gmail.com>
2022-11-24 17:48:40 +0100

    init

---
+++ 20221124_OOP/Utente.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20221124_OOP
+{
+    internal static class Utente
+    {
+        private static string _username;
+        public static string Username
+        {
+            get { return _username; }
+            set { _username = "user"; }
+        }
+
+        private static string _password;
+
+        public static string Password
+        {
+            get { return _password;}
+            set { _password = "pass"; }
+        }
+
+        private static bool _login;
+        public static bool Login
+        {
+            get { return _login;}
+            set { _login = fa; }
+        }
+        
+
+    }
+}
